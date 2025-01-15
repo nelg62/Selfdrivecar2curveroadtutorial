2f511a5d5a84795d07afe29f4a7cd4f6b26e38fe
nelg62 <glen.harding.nz@gmail.com>
2025-01-15 16:43:21 +1300

    taking break up to neural networks

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
 https://www.youtube.com/watch?v=NUjN2Mln_Gg&list=PLB0Tybl0UNfYoJE7ZwsBQoDIG4YN9ptyY&index=11
 
-up to 2:02:13
+up to 3:36:29
--- main.js
+++ main.js
@@ -3,18 +3,24 @@ myCanvas.width = 200;
 
 const ctx = myCanvas.getContext("2d");
 const road = new Road(myCanvas.width / 2, myCanvas.width * 0.9);
-const car = new Car(100, 100, 30, 50);
+const car = new Car(100, 100, 30, 50, "KEYS");
+const traffic = [new Car(100, -100, 30, 50, "DUMMY", 2)];
 
 animate();
 
 function animate() {
-  car.update();
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].update([], []);
+  }
+  car.update(road.borders, traffic);
 
   myCanvas.height = window.innerHeight;
 
   ctx.translate(0, -car.y + myCanvas.height * 0.7);
-  car.update();
   road.draw(ctx);
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].draw(ctx);
+  }
   car.draw(ctx);
 
   requestAnimationFrame(animate);
